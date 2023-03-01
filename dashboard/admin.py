76d79ada2fae2850e3de5173a7f5dd66e4700445
Welley <monteirowelley7@gmail.com>
2023-03-01 16:45:49 -0300

    config admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ListNotification
 
-# Register your models here.
+class ListListNotification(admin.ModelAdmin):
+    list_display = ('description','stats', 'hour')
+
+admin.site.register(ListNotification, ListListNotification)
--- dashboard/models.py
+++ dashboard/models.py
@@ -4,3 +4,6 @@ class ListNotification(models.Model):
     description = models.CharField('Descrição', max_length=50)
     stats = models.CharField('Estatos', max_length=20)
     hour = models.IntegerField('Horas')
+
+    def __str__(self):
+        return self.description
