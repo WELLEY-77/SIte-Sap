fe1952dddb4f8f42b230d908f994b67e8a0863a2
Welley <monteirowelley7@gmail.com>
2023-03-01 16:57:15 -0300

    add models ao index

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import ListNotification
 
 def dashboard(request):
-    return render(request, 'dashboard/index.html')
+
+    lista_notificao = ListNotification.objects.all()
+
+    dados = {
+        'lista_notificacao':lista_notificao
+    }
+
+    return render(request, 'dashboard/index.html', dados)
