6f840f4fd4a1f2161e0fee3455e40ca88d37db0b
Welley <monteirowelley7@gmail.com>
2023-03-02 13:46:44 -0300

    criando banco de dados e admin

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ListProdutos(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    unidade = models.IntegerField('Unidades vendidas')
+    em_estoque = models.CharField('Em estoque', max_length=8)
+    date = models.DateField('Data', auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return f'{self.name}'
